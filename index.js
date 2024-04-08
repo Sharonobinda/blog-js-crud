3e1c11c7aeef1cb87a74535775789f367041b2b7
k-koech <koechkelvin97@gmail.com>
2024-04-08 09:55:08 +0300

    deploy

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function editPost(id)
      const update_container = document.getElementById("update_container")
 
      update_container.innerHTML = `
-     <h4>Update Post</h4>
+     <h4 class="text-center">Update Post</h4>
      <div class="max-w-md mx-auto">
      <div class="relative z-0 w-full mb-5 group">
          <input type="text" value="${post.title}" id="title_update" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
